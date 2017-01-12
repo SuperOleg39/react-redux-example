dbe5ea5daab44b0d8b3a39496cb80b100bf489dc
Драпеза Олег <odrapeza@svyaznoy.ru>
2017-01-12 17:56:44 +0300

    fix router

--- src/core/routes/index.js
+++ src/core/routes/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { Route } from 'react-router';
 
 import App from '../../containers/app/App';
-import Form from '../../containers/userForm/UserForm';
+import UserForm from '../../containers/userForm/UserForm';
 import NotFound from '../../components/notFound/NotFound';
 
 const routes = <Route path="/" component={App}>
-    <Route path="form" component={Form} />
+    <Route path="form" component={UserForm} />
     <Route path="*" component={NotFound} />
 </Route>;
 
